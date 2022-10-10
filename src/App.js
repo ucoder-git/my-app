e284ad3dac8b915b60d0b3a200f08f973cd5e2cb
ucoder-git <ucoder.git@gmail.com>
2022-10-11 02:13:15 +0900

    컴포넌트 props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Composition from './components/2-4.Props/Composition';
+import Extraction from './components/2-4.Props/Extraction/Extraction'
+import ClassComponent from './components/2-5.State/ClassComponent';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FunctionalComponent/>
+      <ClassComponent />
+      <Extraction/>
+      <Composition />
     </div>
   );
 }
---
+++ src/components/2-4.Props/Composition.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Welcome(props){
+    return <h1>Hello, {props.name}</h1>;
+}
+
+export default function Composition() {
+  return (
+    <div>
+      
+      <Welcome name="Jimmy"/>
+      <Welcome name="Timmy"/>
+      <Welcome name="Ammy"/>
+      <Welcome name="Simmy"/>
+
+    </div>
+    
+  );
+}
