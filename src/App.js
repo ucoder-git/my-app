69e5e1de435d71f05cea08129d15da44593be2b2
ucoder-git <ucoder.git@gmail.com>
2022-10-12 04:42:30 +0900

    Synthetic Event

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
-import Composition from './components/2-4.Props/Composition';
-import Extraction from './components/2-4.Props/Extraction/Extraction'
-import ClassComponent from './components/2-5.State/ClassComponent';
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import Event from './components/2-7.Event/Event';
+// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
+// import Composition from './components/2-4.Props/Composition';
+// import Extraction from './components/2-4.Props/Extraction/Extraction'
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent2 />
+      <Event/>
+      {/* <ClassComponent2 />
       <FunctionalComponent/>
       <ClassComponent />
       <Extraction/>
-      <Composition />
+      <Composition /> */}
     </div>
   );
 }
---
+++ src/components/2-7.Event/Event.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Event() {
+    const handleButtonClick = () => {
+        console.log('handleButtonClick');
+    }
+
+    const handleClickCapture = () => {
+        console.log('handleClickCapture');
+    }
+    const handleMouseLeave = () => {
+        console.log('handleMouseLeave');
+    }
+
+
+    const handleClickCapture2 = () => {
+        console.log('handleClickCapture2');
+    }
+    
+    const handleClickBubble = () => {
+        console.log('handleClickBubble');
+    }
+  return (
+    <div onClickCapture={handleClickCapture}>
+        <div onClickCapture={handleClickCapture2} onClick={handleClickBubble}>
+      <button onClick={handleButtonClick} onMouseLeave={handleMouseLeave}>Button</button>
+      </div>
+    </div>
+  )
+}
